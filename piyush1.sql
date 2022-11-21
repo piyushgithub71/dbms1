a2cfa7959b4f760439c447e3cf5849fbd7b32a54
piyushgithub71 <113230532+piyushgithub71@users.noreply.github.com>
2022-11-21 14:52:32 +0530

    Create piyush1.sql

---
+++ piyush1.sql
@@ -0,0 +1,19 @@
+create table emp1(e_id  number(5),e_name varchar(10),e_salary number (10));
+insert into  emp1 values (1,'abc',5000);
+insert into  emp1 values (1,'xyz',6000);
+insert into  emp1 values (1,'pqr',7000);
+select* from emp1; 
+
+
+create table emp2(e_id  number(5),e_name varchar(10),e_salary number (10));
+insert into  emp2 values (2,'xyz',6000);
+insert into  emp2 values (3,'pqr',7000);
+insert into  emp2 values (4,'efg',9000);
+select* from emp2; 
+select e_name from  emp1 where e_name like 'x%'
+union
+select e_name from  emp2 where e_name like 'x%';
+
+ 
+
+
